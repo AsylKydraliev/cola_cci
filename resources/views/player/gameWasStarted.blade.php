1d93f21767061282b9b659e232396da67fbcc48c
a.kydraliev <a.kydraliev@kulikov.com>
2023-12-14 15:36:14 +0600

    refactor: game was started page

--- resources/views/player/gameWasStarted.blade.php
+++ resources/views/player/gameWasStarted.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.gameFinish')
 
 @section('content')
-    <h1 class="mt-4">Извините, игра уже началась : (</h1>
+    <img src="{{ asset('images/game-over.png') }}" alt="Game over" width="400">
+    <h1 class="mt-4">Извините, игра уже началась</h1>
 @endsection
