690828107072db4be8bf317a5977c25474a526f4
a.kydraliev <a.kydraliev@kulikov.com>
2023-12-25 17:26:12 +0600

    refactor: Add display text settings for mobile view

--- resources/views/moderator/roundGame.blade.php
+++ resources/views/moderator/roundGame.blade.php
@@ -27,6 +27,17 @@
         <div class="container text-center">
             <img src="{{ asset('images/game-over.png') }}" alt="Game over" width="400">
             <h1 class="mt-4">Извините, страница пока недоступна в этом разрешении экрана</h1>
+            <div class="mt-5 card card-body fs-5">
+                <div>Возможно у Вас в настройках дисплея установлен <strong>размер текста 150%.</strong></div>
+                Если у вас Windows 10 и выше,
+                для изменения этого параметра нажмите в любом пустом месте на рабочем столе правой кнопкой мыши
+
+                <div class="mx-auto text-start">
+                    <div>> выберите <strong>Параметры экрана</strong></div>
+                    <div>> в разделе <strong>Масштаб и Разметка</strong></div>
+                    <div>> укажите<strong> размер текста 100%.</strong></div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
