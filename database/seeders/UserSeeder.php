f7045209b11252a907adec9052632313d1d94d4e
AbdievMarat <abdiev.m.t@gmail.com>
2024-05-03 11:17:15 +0600

    fix: user seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,9 +13,9 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@play4skills.com',
         ]);
-        \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
     }
 }
